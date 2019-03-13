80930028b5d9bb4342eef99c84f1a0cf0bdcf468
robmoore1994 <46931490+robmoore1994@users.noreply.github.com>
2019-03-13 16:10:22 +0000

    Changed correctPps method in EmployeeDetails class using string regex to match project specification

--- Refactoring/src/EmployeeDetails.java
+++ Refactoring/src/EmployeeDetails.java
@@ -653,12 +653,9 @@ public class EmployeeDetails extends JFrame implements ActionListener, ItemListe
 	public boolean correctPps(String pps, long currentByte) {
 		boolean ppsExist = false;
 		// check for correct PPS format based on assignment description
-		if (pps.length() == 8 || pps.length() == 9) {
-			if (Character.isDigit(pps.charAt(0)) && Character.isDigit(pps.charAt(1))
-					&& Character.isDigit(pps.charAt(2))	&& Character.isDigit(pps.charAt(3)) 
-					&& Character.isDigit(pps.charAt(4))	&& Character.isDigit(pps.charAt(5)) 
-					&& Character.isDigit(pps.charAt(6))	&& Character.isLetter(pps.charAt(7))
-					&& (pps.length() == 8 || Character.isLetter(pps.charAt(8)))) {
+		if (pps.length() == 7 ) {
+			if (pps.matches("[0-9][0-9][0-9][0-9][0-9][0-9][a-zA-Z]"))
+			{
 				// open file for reading
 				application.openReadFile(file.getAbsolutePath());
 				// look in file is PPS already in use
